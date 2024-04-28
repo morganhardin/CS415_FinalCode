8c7f07e4eec7f98fd66b78f8026bfb99c2e93b2c
morganhardin <77804074+morganhardin@users.noreply.github.com>
2024-04-28 13:20:24 -0500

    updated files

---
+++ LoRaReceiver_Encrypted.ino
@@ -0,0 +1,183 @@
+/* Heltec Automation Receive communication test example
+ *
+ * Function:
+ * 1. Receive the same frequency band lora signal program
+ *  
+ * Description:
+ * 
+ * HelTec AutoMation, Chengdu, China
+ * 成都惠利特自动化科技有限公司
+ * www.heltec.org
+ *
+ * this project also realess in GitHub:
+ * https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
+ * */
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+
+#include "AESLib.h"
+#define BAUD 9600
+
+AESLib aesLib;
+
+#define INPUT_BUFFER_LIMIT (128 + 1) // designed for Arduino UNO, not stress-tested anymore (this works with readBuffer[129])
+
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+int16_t txNumber;
+
+int16_t rssi,rxSize;
+
+bool lora_idle = true;
+
+unsigned char cleartext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
+unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
+
+unsigned char readBuffer[18] = "username:password";
+
+// AES Encryption Key (same as in node-js example)
+byte aes_key[] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+
+// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
+byte aes_iv[N_BLOCK] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+
+// Generate IV (once)
+void aes_init() {
+  aesLib.gen_iv(aes_iv);
+  aesLib.set_paddingmode((paddingMode)0);
+}
+
+uint16_t encrypt_to_ciphertext(char * msg, uint16_t msgLen, byte iv[]) {
+  Serial.println("Calling encrypt (string)...");
+  // aesLib.get_cipher64_length(msgLen);
+  int cipherlength = aesLib.encrypt((byte*)msg, msgLen, (byte*)ciphertext, aes_key, sizeof(aes_key), iv);
+                   // uint16_t encrypt(byte input[], uint16_t input_length, char * output, byte key[],int bits, byte my_iv[]);
+  return cipherlength;
+}
+
+uint16_t decrypt_to_cleartext(byte msg[], uint16_t msgLen, byte iv[]) {
+  Serial.print("Calling decrypt...; ");
+  uint16_t dec_bytes = aesLib.decrypt(msg, msgLen, (byte*)cleartext, aes_key, sizeof(aes_key), iv);
+  Serial.print("Decrypted bytes: "); Serial.println(dec_bytes);
+  return dec_bytes;
+}
+
+void setup() {
+    Serial.begin(115200);
+    Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+    
+    txNumber=0;
+    rssi=0;
+
+
+
+    aes_init(); // generate random IV, should be called only once? causes crash if repeated...
+  
+    RadioEvents.RxDone = OnRxDone;
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                               LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                               LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                               0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+}
+
+
+/* non-blocking wait function */
+void wait(unsigned long milliseconds) {
+  unsigned long timeout = millis() + milliseconds;
+  while (millis() < timeout) {
+    yield();
+  }
+}
+
+unsigned long loopcount = 0;
+
+// Working IV buffer: Will be updated after encryption to follow up on next block.
+// But we don't want/need that in this test, so we'll copy this over with enc_iv_to/enc_iv_from
+// in each loop to keep the test at IV iteration 1. We could go further, but we'll get back to that later when needed.
+
+// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
+byte enc_iv[N_BLOCK] =      { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+byte enc_iv_to[N_BLOCK]   = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+byte enc_iv_from[N_BLOCK] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+
+
+
+void loop()
+{
+  if(lora_idle)
+  {
+    lora_idle = false;
+    Serial.println("into RX mode");
+    Radio.Rx(0);
+  }
+  Radio.IrqProcess( );
+}
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+{
+    rssi=rssi;
+    rxSize=size;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    Radio.Sleep( );
+   
+   
+    Serial.print("readBuffer length: "); Serial.println(sizeof(readBuffer));
+
+   // must not exceed INPUT_BUFFER_LIMIT bytes; may contain a newline
+    sprintf((char*)cleartext, "%s", readBuffer);
+    Serial.println("Encrypted. Decrypting..."); Serial.println(rxpacket ); Serial.flush();
+  
+    uint16_t msgLen = sizeof(rxpacket);
+    memcpy(enc_iv, enc_iv_to, sizeof(enc_iv_to));
+    uint16_t encLen = encrypt_to_ciphertext((char*)cleartext, msgLen, enc_iv);
+
+    unsigned char base64decoded[50] = {0};
+    base64_decode((char*)base64decoded, (char*)ciphertext, msgLen);
+    
+    memcpy(enc_iv, enc_iv_from, sizeof(enc_iv_from));
+    uint16_t decLen = decrypt_to_cleartext(base64decoded, strlen((char*)base64decoded), enc_iv);
+    Serial.print("Decrypted cleartext of length: "); Serial.println(decLen);
+    Serial.print("Decrypted cleartext:\n"); Serial.println((char*)cleartext);
+
+    Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
+    lora_idle = true;
+
+    if (strcmp((char*)readBuffer, (char*)cleartext) == 0) {
+      Serial.println("Decrypted correctly.");
+    } else {
+      Serial.println("Decryption test failed.");
+    }
+
+    Serial.println("---");
+}
